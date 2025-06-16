7d61852c0ea5181ee4216682f857ada27fd7a496
Audina Tiara <tiaraaudina37@gmail.com>
2025-06-16 07:56:05 +0700

    Update remidi1_Audina_6.py

--- remidi1_Audina_6.py
+++ remidi1_Audina_6.py
@@ -1,13 +1,8 @@
-# remidi1_nama_absen.py
-
-# Inisialisasi penghitung bilangan genap
+# Remidi1_Audina_6.py
 jumlah_genap = 0
-
-# Input 5 bilangan dan langsung cek apakah genap
 for i in range(5):
     angka = int(input(f"Masukkan bilangan ke-{i+1}: "))
     if angka % 2 == 0:
         jumlah_genap += 1
 
-# Tampilkan hasil
-print(f"\nJumlah bilangan genap: {jumlah_genap}")
+print(f"Jumlah bilangan genap: {jumlah_genap}")
